8671b2658af8a6c6d2edbee41045822203411ad1
viram-ms <viram1999@gmail.com>
2020-03-25 01:56:18 +0530

    first commit

---
+++ pollsapi/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .apiviews import PollList,PollDetail, ChoiceList, CreateVote
+
+
+urlpatterns = [
+    # path("polls/",PollList.as_view(),name = "polls_list"),
+    # path("polls/<int:pk>/",PollDetail.as_view(), name="polls_details"),
+    path("polls/<int:pk>/choices/",ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/",CreateVote.as_view(), name="create_vote")
+
+
+]
